84cd49b7baffbe37cd34881f02d6a3f1298302bf
sadxbastard <99790839+sadxbastard@users.noreply.github.com>
2023-09-19 00:41:21 +0500

    Add files via upload

---
+++ TuringMachineMultiplication.cpp
@@ -0,0 +1,339 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+#define TIME 600
+
+enum class States { q0, q1, q2, q3 , q4, q5, q6, q7, q8, q9 };
+
+int main()
+{
+	std::string input;
+	States state = States::q0;
+	bool flag = true;
+	int counter = 0;
+
+	std::cout << std::endl;
+	std::cin >> input;
+	input.insert(0, " ");
+	input.push_back(' ');
+
+	std::string readingHead(input.size(), ' ');
+	system("cls");
+
+	while (flag)
+	{
+		switch (state)
+		{
+		case States::q0:
+			if (input[counter] == '1' or input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q0\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+
+				std::cout << "Next state: q0\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == '*')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q0\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+				state = States::q1;
+
+				std::cout << "Next state: q1\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q1:
+			if (input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q1\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = ' ';
+				counter += 1;
+				state = States::q2;
+
+				std::cout << "Next state: q2\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == '=')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q1\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter -= 1;
+				state = States::q4;
+
+				std::cout << "Next state: q4\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q2:
+			if (input[counter] == '1' or input[counter] == '=')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q2\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+
+				std::cout << "Next state: q2\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				readingHead.push_back(' ');
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q2\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = '1';
+				input.push_back(' ');
+				counter -= 1;
+				state = States::q3;
+
+				std::cout << "Next state: q3\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q3:
+			if (input[counter] == '=' or input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q3\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter -= 1;
+
+				std::cout << "Next state: q3\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q3\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+				state = States::q1;
+
+				std::cout << "Next state: q1\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q4:
+			if (input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q4\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = '1';
+				counter -= 1;
+
+				std::cout << "Next state: q4\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == '*')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q4\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter -= 1;
+				state = States::q5;
+
+				std::cout << "Next state: q5\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q5:
+			if (input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q5\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter -= 1;
+				state = States::q6;
+
+				std::cout << "Next state: q6\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q5\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+				state = States::q8;
+
+				std::cout << "Next state: q8\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q6:
+			if (input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q6\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter -= 1;
+
+				std::cout << "Next state: q6\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if(input[counter] == ' ')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q6\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+				state = States::q7;
+
+				std::cout << "Next state: q7\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q7:
+			if (input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q7\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = ' ';
+				counter += 1;
+				state = States::q8;
+
+				std::cout << "Next state: q8\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q8:
+			if (input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q8\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				counter += 1;
+				state = States::q0;
+
+				std::cout << "Next state: q0\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == '*')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q8\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = ' ';
+				counter += 1; //
+				state = States::q9;
+
+				std::cout << "Next state: q9\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		case States::q9:
+			if (input[counter] == '*' or input[counter] == '1')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q9\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = ' ';
+				counter += 1;
+
+				std::cout << "Next state: q9\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			else if (input[counter] == '=')
+			{
+				readingHead[counter] = '|';
+				std::cout << readingHead << std::endl;
+				readingHead[counter] = ' ';
+				std::cout << input;
+				std::cout << "\n\nCurrent state: q9\nCurrent value: |" << input[counter] << "|" << std::endl << std::endl;
+
+				input[counter] = ' ';
+				flag = false;
+
+				std::cout << "Next state: q9\nNext value: |" << input[counter] << "|" << std::endl;
+				Sleep(TIME);
+				system("cls");
+			}
+			break;
+		}
+	}
+	std::cout << input << std::endl;
+	return 0;
+}
